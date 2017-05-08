7bb41cb98d7a380c48252fc47f5952a7a49d32a8
Mark Overman <markoverman@Marks-MacBook-Air.local>
2017-05-08 10:20:27 -0500

    Assignment 9 dom events completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,15 +1,30 @@
-var animatePoints = function() {
+var pointsArray = document.getElementsByClassName('point');
 
-    var points = document.getElementsByClassName('point');
-
-    var revealPoint = function(index){
-        points[index].style.opacity = 1;
-        points[index].style.transform = "scaleX(1) translateY(0)";
-        points[index].style.msTransform = "scaleX(1) translateY(0)";
-        points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-    }
-
-    for (var i = 0; i < points.length; i++){
-        revealPoint(i);
-    }
+var revealPoint = function(point) {
+    point.style.opacity = 1;
+    point.style.transform = "scaleX(1) translateY(0)";
+    point.style.msTransform = "scaleX(1) translateY(0)";
+    point.style.WebkitTransform = "scaleX(1) translateY(0)";
 };
+
+// I don't understand why points is here as it doesnt reference anything now
+// that point above was changed from points to point 
+var animatePoints = function(points) {
+    forEach(points, revealPoint);
+};
+
+window.onload = function() {
+    // Automatically animate the points on a tall screen where scrolling can't trigger the animation
+     if (window.innerHeight > 950) {
+         animatePoints(pointsArray);
+     }
+     // these variables are not in the end of video at end of assignment
+     var sellingPoints = document.getElementsByClassName('selling-points')[0];
+     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+    window.addEventListener("scroll", function(event) {
+        if (pointsArray[0].getBoundingClientRect().top <= 500) {
+             animatePoints(pointsArray);
+         }
+    });
+}
