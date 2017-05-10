0c9931cc27552d44b135cfbc9c551a175868ec0e
Mark Overman <markoverman@Marks-MacBook-Air.local>
2017-05-10 16:33:35 -0500

    Assignment 11 scripting: completed

--- scripts/album.js
+++ scripts/album.js
@@ -29,6 +29,21 @@
          { title: 'Wrong phone number', duration: '2:15'}
      ]
  };
+ // Another Example Album
+ var albumFamily = {
+     title: 'The Big Event',
+     artist: 'Dylan Kenneth',
+     label: 'MO',
+     year: '1999',
+     albumArtUrl: 'assets/images/album_covers/19.png',
+     songs: [
+         { title: 'Your friend', duration: '1:01' },
+         { title: 'Comes to see you', duration: '5:01' },
+         { title: 'Happy ever after', duration: '3:21'},
+         { title: 'Yes I do', duration: '3:14' },
+         { title: 'I am here', duration: '2:15'}
+     ]
+ };
 
  var createSongRow = function(songNumber, songName, songLength) {
      var template =
@@ -42,15 +57,13 @@
      return template;
  };
 
- var setCurrentAlbum = function(album) {
-     // #1
-     var albumTitle = document.getElementsByClassName('album-view-title')[0];
-     var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-     var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-     var albumImage = document.getElementsByClassName('album-cover-art')[0];
-     var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+ var albumTitle = document.getElementsByClassName('album-view-title')[0];
+ var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+ var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+ var albumImage = document.getElementsByClassName('album-cover-art')[0];
+ var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
 
-     // #2
+ var setCurrentAlbum = function(album) {
      albumTitle.firstChild.nodeValue = album.title;
      albumArtist.firstChild.nodeValue = album.artist;
      albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
@@ -68,3 +81,13 @@
  window.onload = function() {
      setCurrentAlbum(albumPicasso);
  };
+
+ var albums = [albumPicasso, albumMarconi, albumFamily];
+ var index = 1;
+ albumImage.addEventListener("click", function(event) {
+     setCurrentAlbum(albums[index]);
+     index++;
+     if (index == albums.length) {
+         index = 0;
+     }
+ });
